4e47fe1f388781c15b1da9410b4e4850a7e0323a
regueiro23 <152506962+regueiro23@users.noreply.github.com>
2024-01-09 16:34:35 +0100

    Update codepierre.py

--- codepierre.py
+++ codepierre.py
@@ -21,3 +21,24 @@ def ospfconf(routerid, interface, adresseip, ospfarea):
     return ospfstring
 
 print(ospfconf(b"0.0.0.0", b"GigabitEthernet 1/0", b"2001::0", b"0"))
+
+def ibgpripconf(interface,adresseip,id_as,routerid,loopback_neighbors):
+    ibgpripstring = b"enable\rconf t\r"
+    ibgpripstring += interface + b"\r"
+    ibgpripstring = ibgpripstring + b"ipv6 enable\ripv6 address "
+    ibgpripstring += adresseip + b"\r"
+    ibgpripstring = ibgpripstring + b"ipv6 rip 0 enable\rexit\rrouter bgp "
+    ibgpripstring += id_as + b"\r"
+    ibgpripstring = ibgpripstring + b"bgp router-id "
+    ibgpripstring += routerid + b"\r"
+    ibgpripstring = ibgpripstring + b"neighbor "
+    for i in range (0,len(loopback_neighbors)):
+          ibgpripstring += loopback_neighbors[i] + b" remote-as " + id_as + b"\r"
+    for i in range (0,len(loopback_neighbors)):
+          ibgpripstring += b"neighbor " + loopback_neighbors[i] + b" update-source " + interface + b"\r"
+    ibgpripstring = ibgpripstring + b"address-family ipv6 unicast\r"
+    for i in range (0,len(loopback_neighbors)):
+          ibgpripstring += b"neighbor " + loopback_neighbors[i] + b" activate\r"
+    return ibgpripstring
+
+print(ibgpripconf(b"Loopback0",b"2001::0",b"112",b"1.1.1.1",[b"2001::2",b"2001:3",b"2001:4"]))

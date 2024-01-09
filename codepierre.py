5abd75e7e1312477847cd15519c139992e2af61c
regueiro23 <152506962+regueiro23@users.noreply.github.com>
2024-01-09 17:35:47 +0100

    Update codepierre.py

--- codepierre.py
+++ codepierre.py
@@ -24,14 +24,7 @@ print(ospfconf(b"0.0.0.0", b"GigabitEthernet 1/0", b"2001::0", b"0"))
 
 
 def ibgpripconf(interface,adresse_ip,id_AS,router_id,loopback_neighbors):
-    ibgpripstring = b"enable\rconf t\r"
-    ibgpripstring += interface + b"\r"
-    ibgpripstring = ibgpripstring + b"ipv6 enable\ripv6 address "
-    ibgpripstring += adresse_ip + b"\r"
-    ibgpripstring = ibgpripstring + b"ipv6 rip 0 enable\rexit\rrouter bgp "
-    ibgpripstring += id_AS + b"\r"
-    ibgpripstring = ibgpripstring + b"bgp router-id "
-    ibgpripstring += router_id + b"\r"
+    ibgpripstring = b"enable\rconf t\r" + interface + b"\ripv6 enable\ripv6 address " + adresse_ip + b"\ripv6 rip 0 enable\rexit\rrouter bgp " + id_AS + b"\rbgp router-id " + router_id + b"\r"
     for i in range (0,len(loopback_neighbors)):
           ibgpripstring += b"neighbor " + loopback_neighbors[i] + b" remote-as " + id_AS + b"\r"
     for i in range (0,len(loopback_neighbors)):
@@ -45,14 +38,7 @@ print(ibgpripconf(b"Loopback0",b"2001::0",b"112",b"1.1.1.1",[b"2001::2",b"2001:3
 
 
 def ibgpospf(interface,adresse_ip,id_AS,router_id,loopback_neighbors):
-    ibgpospfstring = b"enable\rconf t\rinterface "
-    ibgpospfstring += interface + b"\r"
-    ibgpospfstring = ibgpospfstring + b"ipv6 enable\ripv6 address "
-    ibgpospfstring += adresse_ip + b"\r"
-    ibgpospfstring = ibgpospfstring + b"ipv6 ospf 1 area 0\rexit\rrouter bgp "
-    ibgpospfstring += id_AS + b"\r"
-    ibgpospfstring = ibgpospfstring + b"bgp router-id "
-    ibgpospfstring += router_id + b"\r"
+    ibgpospfstring = b"enable\rconf t\rinterface " + interface + b"\ripv6 enable\ripv6 address " + adresse_ip + b"\ripv6 ospf 1 area 0\rexit\rrouter bgp " + id_AS + b"\rbgp router-id " + router_id + b"\r"
     for i in range (0,len(loopback_neighbors)):
           ibgpospfstring += b"neighbor " + loopback_neighbors[i] + b" remote-as " + id_AS + b"\r"
     for i in range (0,len(loopback_neighbors)):
@@ -66,14 +52,7 @@ print(ibgpripconf(b"Loopback0",b"2001::0",b"112",b"1.1.1.1",[b"2001::2",b"2001:3
 
 
 def ebgpconf(id_AS,router_id,neighbor_ip,AS_voisin):
-    ebgpstring = b"enable\rconf t\rrouter bgp "
-    ebgpstring += id_AS + b"\r"
-    ebgpstring = ebgpstring + b"no bgp default ipv4-unicast\rbgp router-id "
-    ebgpstring += router_id + b"\r"
-    ebgpstring = ebgpstring + b"neighbor "
-    ebgpstring += neighbor_ip + b" remote-as\r" + AS_voisin + b"\r"
-    ebgpstring = ebgpstring + b"address-family ipv6 unicast\rneighbor "
-    ebgpstring += neighbor_ip + b" activate\r"
+    ebgpstring = b"enable\rconf t\rrouter bgp " + id_AS + b"\rno bgp default ipv4-unicast\rbgp router-id " + router_id + b"\rneighbor " + neighbor_ip + b" remote-as\r" + AS_voisin + b"\raddress-family ipv6 unicast\rneighbor " + neighbor_ip + b" activate\r"
     return ebgpstring
 
 print(ebgpconf(b"112",b"1.1.1.1",b"2001::5",b"113"))

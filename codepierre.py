418f062732ec4ac02a9817ca703a014ce7d59f62
regueiro23 <152506962+regueiro23@users.noreply.github.com>
2024-01-09 15:25:34 +0100

    jean je t'aime

--- codepierre.py
+++ codepierre.py
@@ -1,34 +1,14 @@
-import telnetlib
-import gns3fy
+#le if pour savoir si mettre rip ou pas vient avant la fonction
+#
 
-def configurer_router(id, protocole, username, password) :
+port = 5015
 
-	#ouvrir une ocnnexion telnet avec le router
-	tn = telnetlib.Telnet(hostname, port)
+def ripconf(interface,adresseip):
+    ripstring = b"enable\rconf t\ripv6 unicast-routing\ripv6 router rip 0\rredistribute connected\rexit\rinterface "
+    ripstring = ripstring + interface + b"\r"
+    ripstring += b"ipv6 enable\ripv6 address "
+    ripstring = ripstring + adresseip + b"\r"
+    ripstring += b"no shutdown\ripv6 rip 0 enable"
+    return ripstring
 
-	#attendre à que le router réponde
-	time.sleep(2)
-
-	#username
-    tn.read_until(b"Username: ")
-    tn.write(username.encode('ascii') + b"\r")
-
-    #password
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\r")
-
-
-    #configuration en fonction du protocole
-    #fonctions intéressantes
-    for command in commands:
-        tn.write(b"enable\r")
-        time.sleep(1) #attendre à l'execution de la commande pour éviter le chevauchement
-        tn.write(b"cisco\r")
-        time.sleep(1)
-        tn.write(b"conf t\r")
-        time.sleep(1) 
-        ...  
-
-    #fermer la connexion telnet
-    tn.write(b"exit\r")
-    tn.close()
+print(ripconf(b"GigabitEthernet 1/0", b"2001::0"))

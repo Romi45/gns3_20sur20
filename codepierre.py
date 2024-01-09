3b1cbc91e44e32367f98a748f7d8d094c8e8d60c
regueiro23 <152506962+regueiro23@users.noreply.github.com>
2024-01-09 15:35:55 +0100

    Update codepierre.py

--- codepierre.py
+++ codepierre.py
@@ -1,9 +1,4 @@
-#le if pour savoir si mettre rip ou pas vient avant la fonction
-#
-
-port = 5015
-
-def ripconf(interface,adresseip):
+def ripconf(interface, adresseip):
     ripstring = b"enable\rconf t\ripv6 unicast-routing\ripv6 router rip 0\rredistribute connected\rexit\rinterface "
     ripstring = ripstring + interface + b"\r"
     ripstring += b"ipv6 enable\ripv6 address "
@@ -12,3 +7,17 @@ def ripconf(interface,adresseip):
     return ripstring
 
 print(ripconf(b"GigabitEthernet 1/0", b"2001::0"))
+
+
+def ospfconf(routerid, interface, adresseip, ospfarea):
+    ospfstring = b"enable\rconf t\ripv6 unicast-routing\ripv6 router ospf 0\rrouter-id "
+    ospfstring = ospfstring + routerid + b"\r"
+    ospfstring += b"exit\rinterface "
+    ospfstring = ospfstring + interface + b"\r"
+    ospfstring += b"ipv6 enable\ripv6 address "
+    ospfstring = ospfstring + adresseip + b"\r"
+    ospfstring += b"no shutdown\ripv6 ospf "
+    ospfstring = ospfstring + ospfarea + b"\r"
+    return ospfstring
+
+print(ospfconf(b"0.0.0.0", b"GigabitEthernet 1/0", b"2001::0", b"0"))

cfa73e4ddf2903ad292032ab59b6378d5e669857
HippG <133984650+HippG@users.noreply.github.com>
2024-02-01 11:30:02 +0100

    Update conf_commands.py

--- Code/modules/conf_commands.py
+++ Code/modules/conf_commands.py
@@ -77,37 +77,53 @@ def bgpconf(id_AS,router_id,loopback_neighbors, ebgp, advertised_networks,type):
         bgp_string (string) : the bgp configuration string.
     """
 
+    route_map = ""
+    local_pref=""
     bgp_string=""
+
     if type == "client":
-        bgp_string = "route-map COSTUMER_RM_PERMIT permit 10\rset community 100:1789\rexit\r"
-        route_map = "neighbor "+ ebgp[1] +" route-map COSTUMER_RM_PERMIT in\r"
+        bgp_string = "route-map CUSTOMER_RM_PERMIT permit 10\rset local-preference 400\rset community 100:1789\rexit\r"
+        route_map = "neighbor "+ ebgp[1] +" route-map CUSTOMER_RM_PERMIT in\r"
 
     elif type == "provider" or type == "peer":
-        bgp_string = "route-map COSTUMER_RM_DENY permit 10\rmatch community COSTUMER_CL\rexit\rroute-map COSTUMER_RM_DENY deny 20\rexit\rip community-list standard COSTUMER_CL permit 100:1789\r"
-        route_map = "neighbor "+ ebgp[1] +" route-map COSTUMER_RM_DENY out\r"
-    else:
-        route_map = ""
+        bgp_string = "route-map CUSTOMER_RM_DENY permit 10\rmatch community CUSTOMER_CL\rexit\rroute-map CUSTOMER_RM_DENY deny 20\rexit\rip community-list standard CUSTOMER_CL permit 100:1789\r"
+        route_map = "neighbor "+ ebgp[1] +" route-map CUSTOMER_RM_DENY out\r"
+
+        if type=="peer":
+            bgp_string += "route-map PEER_LOCAL_PREF \rset local-preference 300\rexit\r"
+            local_pref = "neighbor "+ebgp[1]+" route-map PEER_LOCAL_PREF in\r"
+
+        else:
+            bgp_string += "route-map PROVIDER_LOCAL_PREF \rset local-preference 200\rexit\r"
+            local_pref = "neighbor "+ebgp[1]+" route-map PROVIDER_LOCAL_PREF in\r"
+        
+
     bgp_string += "router bgp " + id_AS + "\rno bgp default ipv4-unicast\rbgp router-id " + router_id + "\r"
     
     for i in range (0,len(loopback_neighbors)):
         bgp_string += "neighbor " + loopback_neighbors[i] + " remote-as " + id_AS + "\r" + "neighbor " + loopback_neighbors[i] + " update-source Loopback0\r"
     
     if ebgp:
-        bgp_string += "\rneighbor " + ebgp[1] + " remote-as " + ebgp[0] + "\r"
+        bgp_string += "neighbor " + ebgp[1] + " remote-as " + ebgp[0] + "\r"
 
     bgp_string += "address-family ipv6 unicast\r"
-    bgp_string +=  route_map
 
     for i in range (0,len(loopback_neighbors)):
         bgp_string += "neighbor " + loopback_neighbors[i] + " activate\r"
         bgp_string += "neighbor " + loopback_neighbors[i] + " send-community both\r"
 
-
     if ebgp:
         bgp_string += "neighbor " + ebgp[1] + " activate\r"
+        bgp_string += "neighbor " + ebgp[1] + " send-community both\r"
+
+    bgp_string +=  route_map
+    bgp_string +=  local_pref
+
+
 
     for network_addrs in advertised_networks:
         bgp_string += f"network {network_addrs}\r"
 
     bgp_string += "exit\rexit\r"   
+
     return bgp_string
